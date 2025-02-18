69bf11f8aa2c6a9193864f8246167c690a9027cf
andreissh <shkudov.and@gmail.com>
2025-02-18 21:57:47 +0300

    add currency data api request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CurrencyInputComponent } from './currency-input/currency-input.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { BehaviorSubject, switchMap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CurrencyInputComponent],
+  imports: [CommonModule, CurrencyInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  currencies = ['USD', 'EUR', 'RUB', 'GBP', 'CNY'];
-  selectedCurrencyFrom = 'USD';
+  currencies: string[] = [];
+  selectedCurrencyFrom$ = new BehaviorSubject<string>('USD');
   selectedCurrencyTo = 'RUB';
   openDropdown: 'from' | 'to' | null = null;
+
+  private http = inject(HttpClient);
+
+  constructor() {
+    this.selectedCurrencyFrom$
+      .pipe(
+        switchMap((currency) =>
+          this.http.get(
+            `https://v6.exchangerate-api.com/v6/${environment.apiKey}/latest/${currency}`
+          )
+        )
+      )
+      .subscribe((data: any) => {
+        console.log(data);
+        this.currencies = Object.keys(data.conversion_rates);
+      });
+  }
 
   onToggleDropdown(type: 'from' | 'to') {
     this.openDropdown = this.openDropdown === type ? null : type;
@@ -19,7 +40,7 @@ export class AppComponent {
 
   selectCurrency(type: 'from' | 'to', currency: string) {
     if (type === 'from') {
-      this.selectedCurrencyFrom = currency;
+      this.selectedCurrencyFrom$.next(currency);
     } else {
       this.selectedCurrencyTo = currency;
     }
@@ -27,9 +48,8 @@ export class AppComponent {
   }
 
   swapCurrencies() {
-    [this.selectedCurrencyFrom, this.selectedCurrencyTo] = [
-      this.selectedCurrencyTo,
-      this.selectedCurrencyFrom,
-    ];
+    const currentFrom = this.selectedCurrencyFrom$.getValue();
+    this.selectedCurrencyFrom$.next(this.selectedCurrencyTo);
+    this.selectedCurrencyTo = currentFrom;
   }
 }
